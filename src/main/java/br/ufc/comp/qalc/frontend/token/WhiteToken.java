81303f42a4edd6a198fb33e2d871b389ffa95fd0
gabriellbernardes <gabrielbernardes@alu.ufc.br>
2019-03-26 10:06:15 -0300

    alteraçao

--- src/main/java/br/ufc/comp/qalc/frontend/token/WhiteToken.java
+++ src/main/java/br/ufc/comp/qalc/frontend/token/WhiteToken.java
@@ -1,10 +1,10 @@
 package br.ufc.comp.qalc.frontend.token;
 
-public class ComToken extends  Token {
+public class WhiteToken extends  Token {
 
 
-    public ComToken(long line, long start) {
-        super(line, start, "#");
+    public WhiteToken(long line, long start) {
+        super(line, start, " ");
     }
 
     public String getFunctionIdentifier() {
@@ -16,8 +16,6 @@ public class ComToken extends  Token {
 
     @Override
     public String getTokenIdentifier() {
-        return "COM";
+        return "WHITE";
     }
 }
-
-
